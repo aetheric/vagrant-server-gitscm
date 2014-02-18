e7c74a87a1ceb4e34c4f682fb06971db3b1964be
pcummuskey <pcummuskey@bravurasolutions.com>
2014-02-18 15:03:26 +1300

    Finally got gerrit running.

--- puppet/init.pp
+++ puppet/init.pp
@@ -2,11 +2,18 @@
 
 class gitscm {
 
-	package { 'wget': }
+	package { 'wget':
+		ensure => 'installed'
+	}
 
-	class { 'gerrit': 
+	package { 'git':
+		ensure => 'installed'
+	}
+
+	class { 'gerrit':
 		gerrit_version => '2.8',
 		gerrit_java => 'openjdk-7-jdk',
+		gerrit_java_home => "/usr/lib/jvm/java-7-openjdk-${::architecture}/jre"
 	}
 
 }
